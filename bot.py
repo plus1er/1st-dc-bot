0f0df7f06406d91037aab706fecf6a1eddce9c2f
plus1er <coderspringb+1@gmail.com>
2026-07-04 12:07:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Bot is running")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
 def run_server():
     port = int(os.environ.get("PORT", 8080))
     HTTPServer(("0.0.0.0", port), Handler).serve_forever()
